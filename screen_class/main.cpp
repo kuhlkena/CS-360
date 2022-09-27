cb546b134ec0ae5a01afb32fb1174ec621337afe
kuhlkena <kuhlkena@whitman.edu>
2022-09-27 10:12:51 -0700

    finished with HW3

--- screen_class/main.cpp
+++ screen_class/main.cpp
@@ -5,22 +5,22 @@ int main() {
 
     //Workspace for testing ray plane intersect:
     Workspace test1(200,4,4,4);
-    int planeColor[3] = {200,200,0};
+    int planeColor[] = {200,200,0};
     test1.createPlane(Tuple(0,0,10,1), Tuple(0,0,1,0), planeColor);
     test1.render("plane_render.ppm"); // should output a solid black image
 
 
     //Workspace for testing ray sphere intersect:
     Workspace test2(200,4,4,4);
-    int sphereColor[3] = {200,200,0};
+    int sphereColor[] = {200,200,0};
     test2.createSphere(Tuple(0,0,10,1), 2.5, sphereColor);
     test2.render("sphere_render.ppm"); // should output as a single black circle in the center of a white image
 
     //Workspace for testing item ordering:
     Workspace test3(200,4,4,4);
-    int planeColor2[3] = {200,0,0};
-    int planeColor3[3] = {0,200,0};
-    int sphereColor2[3] = {0,0,200};
+    int planeColor2[] = {200,0,0};
+    int planeColor3[] = {0,200,0};
+    int sphereColor2[] = {0,0,200};
     test3.createPlane(Tuple(0,0,10,1), Tuple(0,0,1,0), planeColor2);
     test3.createPlane(Tuple(0,0,10,1), Tuple(0,1,-1,0), planeColor3);
     test3.createSphere(Tuple(0,0,5,1), 1, sphereColor2);
